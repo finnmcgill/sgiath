87051b3c2dc8fa73d50b9f0480ccc2753f455503
Finn McGill <2478426M@student.gla.ac.uk>
2026-01-21 22:58:32 +0000

    Update settings.py
    secret jey

--- sgiath/settings.py
+++ sgiath/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4rl40mfg57o4(7dl0)ra9)f)9-b-mz^r1*cs-&6s+ns^ddbh4%"
+##################################################################
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
